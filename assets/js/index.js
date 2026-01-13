290a055517d28ef50a7050dd6f047b7173edc176
Jonathan Decroix <jonathan.decroix@epitech.eu>
2026-01-13 13:34:38 +0100

    fourth commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,29 @@
 const mode = document.getElementById("mode");
 const iconMode = document.getElementById("icon");
 const htmlElement = document.documentElement;
+
+const form = document.getElementById("form");
+const exp = document.getElementById("exp");
+const comp = document.getElementById("comp");
+const formation = document.getElementById("bloc-formation");
+const experience = document.getElementById("bloc-exp");
+const competence = document.getElementById("bloc-competence");
+const addExp = document.getElementById("add-exp");
+const addForm = document.getElementById("add-form");
+const addComp = document.getElementById("add-comp");
+
+
+addExp.addEventListener('click', function (event) {
+    event.preventDefault();
+    exp.remove(experience);
+})
+
+addForm.addEventListener('click', function (event) {
+    event.preventDefault();
+    form.r
+})
+
+
 mode.addEventListener("click", function () {
     if (htmlElement.getAttribute('data-bs-theme') === 'dark') {
         htmlElement.setAttribute('data-bs-theme', "light");
--- cv.php
+++ cv.php
@@ -1,12 +1,12 @@
 <?php
 
 ob_start();
-include 'cv . php ';
+include 'cv.php';
 $html = ob_get_clean(); // le contenu de cv . php est inséré dans la variable html
 $pdf = new Dompdf(); // création du pdf
 $pdf->loadHtml($html); // chargement du contenu dans Dompdf
-$pdf->setPaper('A4 '); // configuration du format
+$pdf->setPaper('A4'); // configuration du format
 $pdf->render(); // rendu du pdf
-$pdf->stream(" cv . pdf "); // envoi au navigateu
+$pdf->stream("cv.pdf"); // envoi au navigateu
 
 ?>
